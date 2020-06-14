c870b1f2d167f5a8d5d4d308696342556473dfe7
Abdul Basit Abbasi <abbasi097@gmail.com>
2020-06-14 11:19:36 +0400

    Changes Made in states
    states updated as per tutorial assignment

--- src/Room.js
+++ src/Room.js
@@ -1,24 +1,45 @@
 import React, { useState } from 'react';
+import './Room.css';
 
 
 function Room () {
   const [islit, setlit] = useState(false);
-  let [age, setAge] = useState(22);
+  let [temprature, setAge] = useState(72);
 
-function Update () {
-  setlit(!islit);
+function On () {
+  setlit(true);
 }
 
+function Off () {
+  setlit(false);
+}
 
   return (
-    <div>
+    <div className={`room ${islit? "lit" : "dark"}`}>
+      <p>
+        There are two buttons for light with functions of keeping light on and off.<br/>
+        When Light is off, background changes to Dark mode and vice versa.
+      </p>
       This is room {islit ? "lit" : "dark"}
       <br/>
-      age={age}
+      <button onClick={On}> Light On </button>
+      <button onClick={Off}> Light Off </button>   
       <br/>
-      <button onClick={Update}> Toggle Light </button>      
+      
       <br/>
-      <button onClick={() => {setAge(++age);}}> Increase age </button>
+      <br/>
+      <br/>
+      <p>
+        Room Temprature is set on 72, as per assignment and buttons will increase or decreacse<br/>
+        temparture accordingly.
+      </p>
+      <br/>
+      Room Temparture={temprature}   
+      <br/>
+      <button onClick={() => {setAge(++temprature);}}> + temprature </button>
+      <button onClick={() => {setAge(--temprature);}}> - temprature </button>
+
+
     </div>
   )
 }
